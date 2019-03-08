cc7ded19d943f59e8d87bf3819e4bdec7a46f170
Courtenay Eccles <court.eccles@gmail.com>
2019-03-08 07:56:28 -0700

    expose endpoints

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const serverPort = 3500
 
 const app = express()
 
+// Top level middlewares
 app.use(express.json())
+
+// Requests
+app.get('/people', (req, res) => {
+   res.status(200).send('people')
+})
+
+app.get('/planets', (req, res) => {
+   res.status(200).send('planets')
+})
 
 // I would normally store the port number in a .env file however I have omitted this step for simplicity
 app.listen(serverPort, () => console.log(`Server is running on port ${serverPort}`))
