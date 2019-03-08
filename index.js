b615d3f62bde98d902df5a2a8e6f67aa664eb621
Courtenay Eccles <court.eccles@gmail.com>
2019-03-08 08:35:20 -0700

    people enpoint fetches full list

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-const express = require('express')
-const serverPort = 3500
+const express = require("express");
+const axios = require("axios");
+const serverPort = 3500;
 
-const app = express()
+const app = express();
 
 // Top level middlewares
-app.use(express.json())
+// app.use(express.json());
 
 // Requests
-app.get('/people', (req, res) => {
-   res.status(200).send('people')
-})
+app.get("/people", async (req, res) => {
+  const { sortBy } = req.query;
+  const allPeople = [];
+  let swaPeeps = "https://swapi.co/api/people";
+  do {
+    try {
+      const { data } = await axios.get(swaPeeps);
+      data.results.forEach(person => {
+        allPeople.push(person);
+      });
+      swaPeeps = data.next;
+    } catch (error) {
+      console.log(error);
+    }
+  } while (swaPeeps);
 
-app.get('/planets', (req, res) => {
-   res.status(200).send('planets')
-})
+  res.status(200).send(allPeople);
+});
+
+app.get("/planets", (req, res) => {
+  res.status(200).send("planets");
+});
 
 // I would normally store the port number in a .env file however I have omitted this step for simplicity
-app.listen(serverPort, () => console.log(`Server is running on port ${serverPort}`))
+app.listen(serverPort, () =>
+  console.log(`Server is running on port ${serverPort}`)
+);
